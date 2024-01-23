1be614cfc5ab5e087c4323388ef123697223a5d1
zachyunl <jirizachar@seznam.cz>
2024-01-23 20:34:30 +0100

    RandomText

--- src/components/RandomText.js
+++ src/components/RandomText.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import './RandomText.css';
 
 const texts = [
-  'První text.',
-  'Druhý text.',
-  'Třetí text.',
+  'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  'Lorem Ipsum has been the industry s standard dummy text ever since the 1500s',
+  'when an unknown printer took a galley of type and scrambled it to make a type specimen book',
 ];
 
 const RandomText = () => {
@@ -18,10 +19,15 @@ const RandomText = () => {
     setRandomText(getRandomText());
   }, []);
 
+  const prefixes = ['první text', 'druhý text', 'třetí text'];
+  const prefixColor = 'green';
+
   return (
     <div>
       <h1>Náhodný text:</h1>
-      <p>{randomText}</p>
+      <p>
+        <span style={{ color: prefixColor }}>{prefixes[texts.indexOf(randomText)]}</span> {randomText}
+      </p>
     </div>
   );
 };

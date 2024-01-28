1ba39d1706478c08f6067b43b448a28f7680d2d3
zachyunl <jirizachar@seznam.cz>
2024-01-28 20:57:50 +0100

    Change ALT_StaticComponent

--- src/components/StaticComponent.js
+++ src/components/StaticComponent.js
@@ -3,7 +3,7 @@ import './StaticComponent.css';
 const StaticComponent = ({ imagePath, text }) => {
     return (
       <div>
-        <img src={imagePath} alt="Static Image" style={{ width: '100%', maxHeight: '200px', objectFit: 'cover' }} />
+        <img src={imagePath} alt="Obrázek sportu" style={{ width: '100%', maxHeight: '200px', objectFit: 'cover' }} />
         <p>{text}</p>
       </div>
     );
